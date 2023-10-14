2f4dd91e38319bc375b3737c24daf5137a048f85
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-14 15:57:51 -0300

    settings buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import Settings from './Settings';
+import SettingsContext from './SettingsContext';
 import Timer from "./Timer";
+import { useState } from "react";
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
     <main>
-      <Timer />
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> :  <Timer />}
+      </SettingsContext.Provider>
     </main>
   );
 }
