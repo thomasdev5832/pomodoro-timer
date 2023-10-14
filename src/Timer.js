fb8939306624006a37b6eaea6a9bee67f30e951e
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-14 02:11:27 -0300

    first commit

---
+++ src/Timer.js
@@ -0,0 +1,28 @@
+import { CircularProgressbar, buildStyles  } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+
+const red = '#f54e4e';
+const green = '#4aec8c'
+
+function Timer () {
+    return (
+        <div>
+            <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+                rotation:0, 
+                strokeLinecap: 'butt',
+                textColor: '#fff',
+                pathColor: red,
+                tailColor: 'rgba(255,255,255,.2',
+            })} />
+            <div style={{margin:'1em'}}>
+                <PlayButton />
+                <PauseButton />
+            </div>
+        </div>
+        
+    );
+}
+
+export default Timer;
